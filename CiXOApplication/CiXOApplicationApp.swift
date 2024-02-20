c01e185edf388deed6ed2d20b0fc5c6949e67af0
Thien Nguyen Duc <thien.nguyenduc@axonactive.com>
2024-02-20 17:32:48 +0700

    Initial Commit

---
+++ CiXOApplication/CiXOApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CiXOApplicationApp.swift
+//  CiXOApplication
+//
+//  Created by ndthien01 on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CiXOApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
